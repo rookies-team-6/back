8719ee2002a312d6378c58000bc573dbc22e4c4e
BitedRadish <a980831@naver.com>
2025-05-27 23:50:28 +0900

    :wrench: chore: redis package install

---
+++ src/main/java/com/boanni_back/project/auth/controller/RedisTestController.java
@@ -0,0 +1,10 @@
+package com.boanni_back.project.auth.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class RedisTestController {
+    private final StringRedisTemplate
+}
