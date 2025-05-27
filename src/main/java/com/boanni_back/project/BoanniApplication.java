86ce88eeab7d00af88d73f47189d9e48dfd44612
19013na <19013@naver.com>
2025-05-27 09:31:06 +0900

    first commit

---
+++ src/main/java/com/boanni_back/project/BoanniApplication.java
@@ -0,0 +1,13 @@
+package com.boanni_back.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoanniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoanniApplication.class, args);
+	}
+
+}
