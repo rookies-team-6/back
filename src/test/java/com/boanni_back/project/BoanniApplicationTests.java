f7b79466f0eb1c95016d565eb96cd004d8c68153
19013na <19013@naver.com>
2025-05-27 09:31:06 +0900

    first commit

---
+++ src/test/java/com/boanni_back/project/BoanniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boanni_back.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoanniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
