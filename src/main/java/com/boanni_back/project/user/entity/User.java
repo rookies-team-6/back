5cd9d8915c70c93c51c9c9b170bb33f0b4b9906a
nueij0429 <ssjjjieun0429@gmail.com>
2025-05-28 00:07:26 +0900

    :bug: fix: admin 수정

--- src/main/java/com/boanni_back/project/user/entity/User.java
+++ src/main/java/com/boanni_back/project/user/entity/User.java
@@ -1,8 +1,12 @@
 package com.boanni_back.project.user.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "users")
 public class User {
     @Id
