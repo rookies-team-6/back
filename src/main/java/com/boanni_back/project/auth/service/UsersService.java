8d6daa8dffbe6508035da6308d7810fc3d04c55f
BitedRadish <a980831@naver.com>
2025-05-29 15:16:30 +0900

    :sparkles: feat: 비밀번호 인코딩 완료

--- src/main/java/com/boanni_back/project/auth/service/UsersService.java
+++ src/main/java/com/boanni_back/project/auth/service/UsersService.java
@@ -7,24 +7,26 @@ import com.boanni_back.project.auth.repository.UsersRepository;
 import com.boanni_back.project.exception.BusinessException;
 import com.boanni_back.project.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UsersService {
     private final UsersRepository usersRepository;
+    private final PasswordEncoder passwordEncoder;  // Bean으로 등록된 BCryptPasswordEncoder
 
     public void saveUser(SignUpRequestDTO request){
-
         passwordCheck(request);
         emailDuplicateCheck(request);
         userDuplicateCheck(request);
         employeeTypeCheck(request);
 //        SecurityConfig에서 BCryptPasswordEncoder bean 등록 필요하고
 //        security config 설정한 후에 비밀번호 인코딩 진행
-        Users user=Users.builder().username(request.getUsername()).email(request.getEmail()).password(request.getPassword()).employeeType(request.getEmployeeType()).build();
+        String rawPassword = request.getPassword();
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+        Users user=Users.builder().username(request.getUsername()).email(request.getEmail()).password(encodedPassword).employeeType(request.getEmployeeType()).build();
         usersRepository.save(user);
-
     }
 
     private void employeeTypeCheck(SignUpRequestDTO request) {
--- src/main/java/com/boanni_back/project/config/SecurityConfig.java
+++ src/main/java/com/boanni_back/project/config/SecurityConfig.java
@@ -1,28 +1,75 @@
-//package com.boanni_back.project.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.web.SecurityFilterChain;
-//import static org.springframework.security.config.Customizer.withDefaults;
-//
-//@Configuration
-//@EnableMethodSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//            // csrf 인증 비활성화, 나중에 비활성화 코드 지우기
-//            .csrf(AbstractHttpConfigurer::disable)
-//            .authorizeHttpRequests(auth -> auth
-//                .requestMatchers(HttpMethod.POST, "/api/questions").hasRole("ADMIN")
-//                .anyRequest().permitAll()
-//            )
-//            .httpBasic(withDefaults());
-//        return http.build();
-//    }
-//}
+package com.boanni_back.project.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+//    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+            .cors(cors->cors.configurationSource(corsConfigurationSource()))
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+        // csrf 인증 비활성화, 나중에 비활성화 코드 지우기
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+//                        해당 경로는 모두 접근 가능하다.
+                    .requestMatchers("/auth/login", "/auth/signup", "/h2-console/**").permitAll()
+//                        ADMIN 권한이 있어야 이용 가능하다.
+                    .requestMatchers(HttpMethod.POST, "/api/questions").hasRole("ADMIN")
+//                        이외 요청은 jwt 토큰이 없으면 접근 불가능하다.
+                    .anyRequest().authenticated()
+            );
+//                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // ⭐️ 필수
+        return http.build();
+
+//        .httpBasic(withDefaults());
+    }
+
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration config=new CorsConfiguration();
+//        허용할 도메인만 추후에 명시
+        config.setAllowedOrigins(List.of("*")); // 모든 Origin 허용 (개발용, 배포 시는 제한 권장)
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
+        config.setAllowCredentials(true); // 쿠키 사용 허용 (JWT는 true/false 가능)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
