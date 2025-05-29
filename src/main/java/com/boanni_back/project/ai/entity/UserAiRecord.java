46d6a451ea13685b351bb6d0cb323e61c20c25e0
19013na <19013@naver.com>
2025-05-29 09:56:40 +0900

    :sparkles: feat: 이용자 답변 등록

--- src/main/java/com/boanni_back/project/ai/entity/UserAiRecord.java
+++ src/main/java/com/boanni_back/project/ai/entity/UserAiRecord.java
@@ -1,11 +1,15 @@
-package com.boanni_back.project.gpt.entity;
+package com.boanni_back.project.ai.entity;
 
 import com.boanni_back.project.auth.entity.Users;
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
 
 @Entity
+@Builder
+@Getter
 @Table(name = "user_gpt_record")
-public class UserGptRecord {
+public class UserAiRecord {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -13,11 +17,11 @@ public class UserGptRecord {
     @Column(nullable = false, columnDefinition = "TEXT")
     private String userAnswer;
 
-    @Column(nullable = false, columnDefinition = "TEXT")
-    private String gptAnswer;
+    @Column(columnDefinition = "TEXT")
+    private String aiAnswer;
 
     @Column(nullable = false)
-    private boolean isBookMarked;
+    private boolean isBookMarked = false;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "question_id")
