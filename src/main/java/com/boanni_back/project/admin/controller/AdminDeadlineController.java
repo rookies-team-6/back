3c9ec74c56d1b7dd02f114b7c700409cee08cedf
nueij0429 <ssjjjieun0429@gmail.com>
2025-05-29 15:07:04 +0900

    :recycle: refactor: code refactoring

--- src/main/java/com/boanni_back/project/admin/controller/AdminDeadlineController.java
+++ src/main/java/com/boanni_back/project/admin/controller/AdminDeadlineController.java
@@ -2,24 +2,24 @@ package com.boanni_back.project.admin.controller;
 
 import com.boanni_back.project.admin.controller.dto.AdminDeadlineDto;
 import com.boanni_back.project.admin.service.AdminDeadlineService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/admin/users/deadline")
 public class AdminDeadlineController {
 
     private final AdminDeadlineService adminDeadlineService;
 
-    public AdminDeadlineController(AdminDeadlineService adminDeadlineService) {
-        this.adminDeadlineService = adminDeadlineService;
-    }
-
+    //해당 id 회원의 start/end date 확인 매핑
     @GetMapping("/{id}")
     public ResponseEntity<AdminDeadlineDto> getDeadline(@PathVariable Long id) {
         return ResponseEntity.ok(adminDeadlineService.getDeadline(id));
     }
 
+    //해당 id 회원의 마감 기한 입력 매핑
     @PatchMapping("/{id}")
     public ResponseEntity<Void> updateDeadline(@PathVariable Long id,
                                                @RequestBody AdminDeadlineDto dto) {
--- src/main/java/com/boanni_back/project/admin/controller/AdminProcessController.java
+++ src/main/java/com/boanni_back/project/admin/controller/AdminProcessController.java
@@ -2,6 +2,7 @@ package com.boanni_back.project.admin.controller;
 
 import com.boanni_back.project.admin.controller.dto.AdminProcessDto;
 import com.boanni_back.project.admin.service.AdminProcessService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,15 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/admin/users")
 public class AdminProcessController {
 
     private final AdminProcessService adminProcessService;
 
-    public AdminProcessController(AdminProcessService adminProcessService) {
-        this.adminProcessService = adminProcessService;
-    }
-
+    //해당 id 회원의 학습 진행률 매핑
     @GetMapping("/progress/{id}")
     public ResponseEntity<AdminProcessDto> getProgress(@PathVariable Long id) {
         return ResponseEntity.ok(adminProcessService.getUserProgress(id));
