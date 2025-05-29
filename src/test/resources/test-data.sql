494be7e8bf3acc0569179ec3701680db633c01c8
nueij0429 <ssjjjieun0429@gmail.com>
2025-05-29 14:38:59 +0900

    :sparkles: feat: admin-progress & deadline

--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -1,8 +1,8 @@
-INSERT INTO users (email, password, username, score, current_question_index, employee_type)
+INSERT INTO users (email, password, username, score, current_question_index, employee_type, start_date, end_date)
 VALUES
-('trainee1@boanni.com', 'pw1', '수강생1', 10, 0, 'TRAINEE'),
-('emp1@boanni.com', 'pw2', '사원1', 2, 0, 'EMPLOYEE'),
-('admin@boanni.com', 'pw3', '관리자', 0, 0, 'ADMIN'),
-('trainee2@boanni.com', 'pw4', '수강생2', 30, 0, 'TRAINEE'),
-('trainee3@boanni.com', 'pw5', '수강생3', 45, 0, 'TRAINEE'),
-('emp2@boanni.com', 'pw6', '사원2', 60, 0, 'EMPLOYEE');
+('trainee1@boanni.com', 'pw1', '수강생1', 10, 1, 'TRAINEE', NULL, NULL),
+('emp1@boanni.com', 'pw2', '사원1', 2, 1, 'EMPLOYEE', NULL, NULL),
+('admin@boanni.com', 'pw3', '관리자', 0, 0, 'ADMIN', NULL, NULL),
+('trainee2@boanni.com', 'pw4', '수강생2', 30, 2, 'TRAINEE', NULL, NULL),
+('trainee3@boanni.com', 'pw5', '수강생3', 45, 2, 'TRAINEE', NULL, NULL),
+('emp2@boanni.com', 'pw6', '사원2', 60, 3, 'EMPLOYEE', NULL, NULL);
