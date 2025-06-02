c44be8d4ceeaedf0eabcb5bf4fe9591f25fba480
BitedRadish <a980831@naver.com>
2025-06-02 10:09:33 +0900

    :art: style: admin 주석 제거

--- src/main/java/com/boanni_back/project/admin/service/AdminScoreService.java
+++ src/main/java/com/boanni_back/project/admin/service/AdminScoreService.java
@@ -19,21 +19,21 @@ public class AdminScoreService {
         this.adminRepository = adminRepository;
     }
 
-//    public List<AdminScoreDto> getAllUserScores() {
-//        return adminRepository.findAll().stream()
-//                .map(u -> new AdminScoreDto(u.getId(), u.getUsername(), u.getScore()))
-//                .collect(Collectors.toList());
-//    }
-//
-//    public AdminScoreDto getUserScoreById(Long id) {
-//        Users users = adminRepository.findById(id)
-//                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다."));
-//        return new AdminScoreDto(users.getId(), users.getUsername(), users.getScore());
-//    }
-//
-//    public List<AdminScoreDto> getScoresSortedDesc() {
-//        return adminRepository.findAllByOrderByScoreDesc().stream()
-//                .map(u -> new AdminScoreDto(u.getId(), u.getUsername(), u.getScore()))
-//                .collect(Collectors.toList());
-//    }
+    public List<AdminScoreDto> getAllUserScores() {
+        return adminRepository.findAll().stream()
+                .map(u -> new AdminScoreDto(u.getId(), u.getUsername(), u.getScore()))
+                .collect(Collectors.toList());
+    }
+
+    public AdminScoreDto getUserScoreById(Long id) {
+        Users users = adminRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다."));
+        return new AdminScoreDto(users.getId(), users.getUsername(), users.getScore());
+    }
+
+    public List<AdminScoreDto> getScoresSortedDesc() {
+        return adminRepository.findAllByOrderByScoreDesc().stream()
+                .map(u -> new AdminScoreDto(u.getId(), u.getUsername(), u.getScore()))
+                .collect(Collectors.toList());
+    }
 }
