6ff52770106d1fd9c09b1c457ae08415df8e3fe7
BitedRadish <a980831@naver.com>
2025-06-04 09:47:21 +0900

    :sparkles: feat: 게시글 작성 DTO에 boardId 추가 및 단일 게시글 조회 mine 속성 추가

--- src/main/java/com/boanni_back/project/board/controller/dto/SingleBoardResponseDTO.java
+++ src/main/java/com/boanni_back/project/board/controller/dto/SingleBoardResponseDTO.java
@@ -1,7 +1,6 @@
 package com.boanni_back.project.board.controller.dto;
 
 import com.boanni_back.project.board.entity.Board;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -9,23 +8,25 @@ import java.time.LocalDateTime;
 @Data
 public class SingleBoardResponseDTO {
     private Long id;
-    @NotBlank
+
     private String username;
 
-    @NotBlank
     private String title;
 
-    @NotBlank
     private String contents;
 
-    @NotBlank
     private LocalDateTime createdAt;
 
-    public SingleBoardResponseDTO(Board board) {
+//    현재 게시글이 로그인한 유저 id와 동일하지 유무 (boolean)
+    private boolean isMine;
+
+
+    public SingleBoardResponseDTO(Board board,Long userId) {
         this.id = board.getId();
         this.username=board.getUsers().getEmployeeNumber().getUsername();
         this.title = board.getTitle();
         this.contents = board.getContents();
         this.createdAt = board.getCreatedAt();
+        this.isMine=board.getUsers().getId().equals(userId);
     }
 }
