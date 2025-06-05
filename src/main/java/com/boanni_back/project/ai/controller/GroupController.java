8c119cebd36add565bd0ad1c5b6a0aab93fea4e1
nueij0429 <ssjjjieun0429@gmail.com>
2025-06-05 12:59:18 +0900

    :recycle: refactor: question code refactoring

--- src/main/java/com/boanni_back/project/ai/controller/GroupController.java
+++ src/main/java/com/boanni_back/project/ai/controller/GroupController.java
@@ -1,9 +1,7 @@
 package com.boanni_back.project.ai.controller;
 
 import com.boanni_back.project.admin.service.AdminScoreService;
-import com.boanni_back.project.ai.controller.dto.GroupAverageScoreDto;
 import com.boanni_back.project.ai.controller.dto.GroupDto;
-import com.boanni_back.project.ai.entity.Group;
 import com.boanni_back.project.ai.service.GroupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,26 +29,5 @@ public class GroupController {
     @GetMapping("/{groupNum}")
     public ResponseEntity<GroupDto.Response> getGroupInfo(@PathVariable Long groupNum) {
         return ResponseEntity.ok(groupService.getGroupInfoByGroupNum(groupNum));
-    }
-
-    // 조별 평균 점수 모두 조회
-    @GetMapping("/average-score")
-    public ResponseEntity<List<GroupAverageScoreDto.Response>> getGroupAverageScores() {
-        Map<Long, Integer> avgMap = groupService.getAverageScoreByGroupNum();
-
-        List<GroupAverageScoreDto.Response> result = avgMap.entrySet().stream()
-                .map(entry -> GroupAverageScoreDto.Response.builder()
-                        .groupNum(entry.getKey())
-                        .averageScore(entry.getValue())
-                        .build())
-                .toList();
-
-        return ResponseEntity.ok(result);
-    }
-
-    // 조별 평균 점수 조회
-    @GetMapping("/{groupNum}/average")
-    public ResponseEntity<GroupAverageScoreDto.Response> getAverageScore(@PathVariable Long groupNum) {
-        return ResponseEntity.ok(groupService.getGroupAverageScore(groupNum));
     }
 }
