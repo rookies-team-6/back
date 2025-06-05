dbe0f03b8e9135235e037f082a10ee563aa8e956
19013na <19013@naver.com>
2025-06-05 09:43:31 +0900

    :wrench: chore: home 조회 기능 - 조별 점수 조회 코드 변경

--- src/main/java/com/boanni_back/project/ai/service/UserService.java
+++ src/main/java/com/boanni_back/project/ai/service/UserService.java
@@ -4,6 +4,7 @@ import com.boanni_back.project.auth.controller.dto.UsersDto;
 import com.boanni_back.project.auth.entity.Users;
 import com.boanni_back.project.auth.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -11,6 +12,7 @@ import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class UserService {
 
     private final UsersRepository usersRepository;
@@ -20,10 +22,25 @@ public class UserService {
                 .orElseThrow(() -> new RuntimeException("사용자 없음"));
 
         // 모든 조별 점수 계산
-        List<Object[]> groupScoresRaw = usersRepository.findAllGroupScores();
-        List<UsersDto.GroupScoreDto> groupScores = groupScoresRaw.stream()
-                            .map(row -> new UsersDto.GroupScoreDto((Long)row[0], ((Number)row[1]).intValue()))
-                            .collect(Collectors.toList());
+        List<Users> allUsers = usersRepository.findAll();
+
+        // 조별 점수 합계와 인원수 집계
+        Map<Long, List<Integer>> groupScoresMap = new HashMap<>();
+        for (Users u : allUsers) {
+            groupScoresMap
+                    .computeIfAbsent(u.getGroupNum(), k -> new ArrayList<>())   // 기존에 있는 경우
+                    .add(u.getScore());
+        }
+
+        // 조별 점수 평균 계산
+        List<UsersDto.GroupScoreDto> groupScores = groupScoresMap.entrySet().stream()
+                .map(entry -> {
+                    Long groupNum = entry.getKey();
+                    List<Integer> scores = entry.getValue();
+                    int avg = scores.stream().mapToInt(Integer::intValue).sum() / scores.size();
+                    return new UsersDto.GroupScoreDto(groupNum, avg);
+                })
+                .collect(Collectors.toList());
 
         // 사용자 정보 DTO 생성
         UsersDto.UserHomeResponse userDto = new UsersDto.UserHomeResponse(
