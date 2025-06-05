fc6c2bd2513371da0c9cc3b2f102c7e0de41139a
nueij0429 <ssjjjieun0429@gmail.com>
2025-06-05 10:17:36 +0900

    :sparkles: feat: groupNum info & average score 1

--- src/main/java/com/boanni_back/project/ai/repository/QuestionRepository.java
+++ src/main/java/com/boanni_back/project/ai/repository/QuestionRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
     Page<Question> findByQuestionContaining(String keyword, Pageable pageable);
+    long count();
 }
