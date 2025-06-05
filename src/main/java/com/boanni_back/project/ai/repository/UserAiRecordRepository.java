a8fba5e11ef58d89a229189d2e4e4248a6080c7d
19013na <19013@naver.com>
2025-06-05 16:03:46 +0900

    :recycle: refactor: 자신이 푼 문제 조회 N+1 문제 해결

--- src/main/java/com/boanni_back/project/ai/repository/UserAiRecordRepository.java
+++ src/main/java/com/boanni_back/project/ai/repository/UserAiRecordRepository.java
@@ -2,6 +2,7 @@ package com.boanni_back.project.ai.repository;
 
 import com.boanni_back.project.ai.entity.UserAiRecord;
 import com.boanni_back.project.auth.entity.Users;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,7 +16,9 @@ public interface UserAiRecordRepository extends JpaRepository<UserAiRecord, Long
     @Query("SELECT uar FROM UserAiRecord uar JOIN FETCH uar.question")
     List<UserAiRecord> findAllWithQuestion();
 
+    @EntityGraph(attributePaths = {"question"})
     Optional<List<UserAiRecord>> findByUsersIdAndQuestionIdLessThanEqualOrderByQuestionIdAsc(Long userId, Long questionId);
+
     Optional<List<UserAiRecord>> findByUsersIdAndIsBookMarkedTrue(Long userId);
 
     List<UserAiRecord> findAllByUsersIn(List<Users> users);
