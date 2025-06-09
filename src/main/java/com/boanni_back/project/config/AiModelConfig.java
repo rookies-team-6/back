c16fd9a64ece883de705139db4e674c887480dcb
19013na <19013@naver.com>
2025-06-09 11:41:44 +0900

    :recycle: refactor: 프롬프트 수정

--- src/main/java/com/boanni_back/project/config/AiModelConfig.java
+++ src/main/java/com/boanni_back/project/config/AiModelConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
+import java.util.Map;
 
 @Configuration
 public class AiModelConfig {
